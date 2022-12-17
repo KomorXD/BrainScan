01462822cafbfd607d6786a2481a0436e027b1cf
Bartłomiej Gordon <bartekk.gordon@gmail.com>
2022-12-17 22:14:40 +0100

    Get pixel values from image

--- BrainScan/src/scan/Scan.cpp
+++ BrainScan/src/scan/Scan.cpp
@@ -1,12 +1,12 @@
 #include "Scan.hpp"
-#include "SimpleITK-2.3/SimpleITK.h"
+
 #include <assert.h>
 #include <typeinfo>
 #include <cstdlib>
 #include <string>
-#include "../Core.hpp"
+#include <iostream>
 
-
+namespace sitk = itk::simple;
 bool Scan::SaveToFile(std::string inputImageFileName)
 {
 
@@ -15,16 +15,66 @@ bool Scan::SaveToFile(std::string inputImageFileName)
 
 bool Scan::LoadFromFile(std::string inputImageFileName)
 {
-	itk::simple::ImageFileReader reader;
-	reader.SetImageIO("");
-	reader.SetFileName(inputImageFileName);
 
-	itk::simple::Image image;
-	image = reader.Execute();
+	sitk::ImageFileReader reader;
+	reader.SetFileName(std::string(inputImageFileName));
+	sitk::Image image = reader.Execute();
 
-	LOG_INFO("GetWidth {}",image.GetWidth());
-	LOG_INFO("GetHeight {}",image.GetHeight());
-	LOG_INFO("GetDepth {}",image.GetDepth());
+	//
+	// First, define the typedefs that correspond to the types of the input
+	// image. This requires foreknowlege of the data type of the input image.
+	//
+	const unsigned int                                 Dimension = 3;
+	typedef float                                      InternalPixelType;
+	typedef itk::Image< InternalPixelType, Dimension > InternalImageType;
+
+	//
+	// We must check the image dimension and the pixel type of the
+	// SimpleITK image match the ITK image we will cast to.s
+	//
+	if (image.GetDimension() != Dimension)
+	{
+		LOG_CRITICAL("Input image is not a {} dimensional image as expected!", Dimension);
+		return false;
+	}
+
+	//
+	// The read sitk::Image could be any pixel type. Cast the image, to
+	// float so we know what type we have.
+	//
+	sitk::CastImageFilter caster;
+	caster.SetOutputPixelType(sitk::sitkInt32);
+	m_image = caster.Execute(image);
+	
+	m_depth = m_image.GetDepth();
+	m_height = m_image.GetHeight();
+	m_width = m_image.GetWidth();
+
+	PrintBuffer();
+
+	LOG_INFO("GetWidth {}", m_image.GetWidth());
+	LOG_INFO("GetHeight {}", m_image.GetHeight());
+	LOG_INFO("GetDepth {}", m_image.GetDepth());
+
 
     return false;
+}
+
+void Scan::PrintBuffer()
+{
+	std::vector<unsigned int> pixelIndex;
+	for (int d = 0; d < m_depth; d++)
+	{
+		for (int h = 0; h < m_height; h++)
+		{
+			for (int w = 0; w < m_width; w++)
+			{
+				pixelIndex = { { static_cast<unsigned int>(w),
+								 static_cast<unsigned int>(h),
+								 static_cast<unsigned int>(d)
+								} };
+				//myfile << m_image.GetPixelAsInt32(pixelIndex) << ";";
+			}
+		}
+	}
 }
--- BrainScan/src/scan/Scan.hpp
+++ BrainScan/src/scan/Scan.hpp
@@ -1,15 +1,24 @@
 #pragma once
 #include <vector>
-
+#include "SimpleITK-2.3/SimpleITK.h"
+#include "../include/SimpleITK-2.3/sitkImageOperators.h"
 #include "Serializable.hpp"
 #include "sequence/ISequence.hpp"
+#include "../Core.hpp"
 
+namespace sitk = itk::simple;
 class Scan : public Serializable
 {
 private:
-	std::vector<ISequence> sequences;
+	sitk::Image m_image;
+	int m_depth = 0;
+	int m_height = 0;
+	int m_width = 0;
 
 public:
 	virtual bool SaveToFile(std::string FileName) override;
 	virtual bool LoadFromFile(std::string inputImageFileName) override;
+
+private:
+	void PrintBuffer();
 };
