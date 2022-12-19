9d5a14c2c489ed0e4897937b37d168d9dd8a3a23
Bartłomiej Gordon <bartekk.gordon@gmail.com>
2022-12-19 09:36:04 +0100

    Minor changes to scan

--- BrainScan/src/scan/Scan.cpp
+++ BrainScan/src/scan/Scan.cpp
@@ -15,48 +15,39 @@ bool Scan::SaveToFile(std::string inputImageFileName)
 
 bool Scan::LoadFromFile(std::string inputImageFileName)
 {
-
-	sitk::ImageFileReader reader;
-	reader.SetFileName(std::string(inputImageFileName));
-	sitk::Image image = reader.Execute();
-
-	//
-	// First, define the typedefs that correspond to the types of the input
-	// image. This requires foreknowlege of the data type of the input image.
-	//
 	const unsigned int                                 Dimension = 3;
 	typedef float                                      InternalPixelType;
 	typedef itk::Image< InternalPixelType, Dimension > InternalImageType;
 
-	//
-	// We must check the image dimension and the pixel type of the
-	// SimpleITK image match the ITK image we will cast to.s
-	//
-	if (image.GetDimension() != Dimension)
+	try
 	{
-		LOG_CRITICAL("Input image is not a {} dimensional image as expected!", Dimension);
-		return false;
+		sitk::ImageFileReader reader;
+		reader.SetFileName(std::string(inputImageFileName));
+		sitk::Image image = reader.Execute();
+
+		if (image.GetDimension() != Dimension)
+		{
+			LOG_CRITICAL("Input image is not a {} dimensional image as expected!", Dimension);
+			return false;
+		}
+
+		sitk::CastImageFilter caster;
+		caster.SetOutputPixelType(sitk::sitkLabelUInt8);
+		m_image = caster.Execute(image);
+		sitk::WriteImage(m_image, "blob.png");
+	}
+	catch (const std::exception& e)
+	{
+		LOG_ERROR("{}", e.what());
 	}
 
-	//
-	// The read sitk::Image could be any pixel type. Cast the image, to
-	// float so we know what type we have.
-	//
-	sitk::CastImageFilter caster;
-	caster.SetOutputPixelType(sitk::sitkInt32);
-	m_image = caster.Execute(image);
 	
 	m_depth = m_image.GetDepth();
 	m_height = m_image.GetHeight();
 	m_width = m_image.GetWidth();
 
-	PrintBuffer();
-
-	LOG_INFO("GetWidth {}", m_image.GetWidth());
-	LOG_INFO("GetHeight {}", m_image.GetHeight());
-	LOG_INFO("GetDepth {}", m_image.GetDepth());
-
-
+	//PrintBuffer();
+	//sitk::WriteImage(m_image, "blob.png");
     return false;
 }
 
