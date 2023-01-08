dba7a92cdb54bf3c589fc4021c5db0a3873325d1
KomorXD <pawel.kupczak.sc@gmail.com>
2023-01-08 19:24:43 +0100

    shrug

--- BrainScan/src/Main.cpp
+++ BrainScan/src/Main.cpp
@@ -1,14 +1,11 @@
 #include "App.hpp"
 #include "Core.hpp"
-#include "scan/Scan.hpp"
 
 int main(int argc, char** argv)
 {
 	Logger::Init();
-	//App::GetInstance().Run();	
-
-	Scan s;
-	s.LoadFromFile("D:\\Projekty\\BrainScan\\NiftiFiles\\ircad_e01_orig.nii");
+	
+	App::GetInstance().Run();	
 
 	return 0;
 }
