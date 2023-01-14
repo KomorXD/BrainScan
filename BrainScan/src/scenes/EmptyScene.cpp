3a8747b5e1a4c3e5978e5c5a4b10c2afe9e37d92
lilboisoul <kdusza2001@gmail.com>
2023-01-14 20:46:14 +0100

    Remove rendering of tool and scan in empty scene

--- BrainScan/src/scenes/EmptyScene.cpp
+++ BrainScan/src/scenes/EmptyScene.cpp
@@ -1,5 +1,6 @@
 #include "EmptyScene.hpp"
 #include "imgui/imgui.h"
+#include "imgui/ImGuiFileDialog.h"
 #include "../Core.hpp"
 
 #include "../ui/UIMenuBar.hpp"
@@ -7,14 +8,16 @@
 #include "../ui/UIToolSettings.hpp"
 #include "../ui/UIScanImageWindow.hpp"
 
+
 const std::string bartekLocation = "D:\\Projekty\\BrainScan\\NiftiFiles\\test.nii";
 const std::string pawelLocation = "E:\\Projects\\ASFDA\\BrainScan\\test_nifti.nii";
+const std::string kubaLocation = "C:\\Users\\kdusz\\Desktop\\BrainScan\\test.nii";
 
 EmptyScene::EmptyScene()
 {
 	FUNC_PROFILE();
 
-	m_Scan.LoadFromFile(bartekLocation);
+	m_Scan.LoadFromFile(kubaLocation);
 
 	m_FB.UnbindBuffer();
 
@@ -47,7 +50,7 @@ EmptyScene::EmptyScene()
 	vbo.Unbind();
 	m_IBO->Unbind();
 
-	View* v = m_Scan.GetSagittal();
+	View* v = m_Scan.GetAxial();
 	
 	m_Shader  = std::make_unique<Shader>("res/shaders/TextureShader.vert", "res/shaders/TextureShader.frag");
 	m_AxialTexture = std::make_shared<Texture>(v->GetData()[40], v->GetWidth(), v->GetHeight());
@@ -55,22 +58,20 @@ EmptyScene::EmptyScene()
 	m_Panels.emplace_back(std::make_unique<UIMenuBar>());
 	m_Panels.emplace_back(std::make_unique<UIToolBar>(m_Panels.back()->GetPosX(), m_Panels.back()->GetPosY() + m_Panels.back()->GetHeight()));
 
-	for(int i = 0; i < 10; ++i)
+	auto tb = (UIToolBar*)m_Panels.back().get();
+
+	tb->AddButton([]()
 	{
-		auto tb = (UIToolBar*)m_Panels.back().get();
+		ImGuiFileDialog::Instance()->OpenDialog("ChooseScan", "Open scan file", ".nii", ".", 1, nullptr, ImGuiFileDialogFlags_Modal);
+		LOG_INFO("Button #{} pressed.", 1);
+	});
 
-		tb->AddButton([i]()
-		{
-			LOG_INFO("Button #{} pressed.", i + 1);
-		});
-	}
+	//m_Panels.emplace_back(std::make_unique<UIToolSettings>(m_Panels.back()->GetPosX(), m_Panels.back()->GetPosY() + m_Panels.back()->GetHeight()));
 
-	m_Panels.emplace_back(std::make_unique<UIToolSettings>(m_Panels.back()->GetPosX(), m_Panels.back()->GetPosY() + m_Panels.back()->GetHeight()));
+	//float scanPanelHeight = m_Panels.back()->GetHeight() / 2.0f;
+	//UIScanImageWindow* scanPanel = nullptr;
 
-	float scanPanelHeight = m_Panels.back()->GetHeight() / 2.0f;
-	UIScanImageWindow* scanPanel = nullptr;
-
-	m_Panels.emplace_back(std::make_unique<UIScanImageWindow>(1, m_Panels.back()->GetPosX() + m_Panels.back()->GetWidth(), m_Panels.back()->GetPosY(),								  scanPanelHeight));
+	/*m_Panels.emplace_back(std::make_unique<UIScanImageWindow>(1, m_Panels.back()->GetPosX() + m_Panels.back()->GetWidth(), m_Panels.back()->GetPosY(), scanPanelHeight));
 	scanPanel = (UIScanImageWindow*)m_Panels.back().get();
 	scanPanel->SetScanTexture(m_AxialTexture);
 
@@ -84,11 +85,14 @@ EmptyScene::EmptyScene()
 
 	m_Panels.emplace_back(std::make_unique<UIScanImageWindow>(4, m_Panels.back()->GetPosX() + m_Panels.back()->GetWidth(), m_Panels.back()->GetPosY(),								  scanPanelHeight));
 	scanPanel = (UIScanImageWindow*)m_Panels.back().get();
-	scanPanel->SetScanTexture(m_AxialTexture);
+	scanPanel->SetScanTexture(m_AxialTexture);*/
+	
 
 	m_FB.BindBuffer();
-	m_FB.AttachTexture((uint32_t)scanPanel->GetWidth() * 3, (uint32_t)scanPanel->GetHeight() * 2);
+	//m_FB.AttachTexture((uint32_t)scanPanel->GetWidth() * 3, (uint32_t)scanPanel->GetHeight() * 2);
+	m_FB.AttachTexture(1, 1);
 	m_FB.UnbindBuffer();
+	
 }
 
 void EmptyScene::Input()
