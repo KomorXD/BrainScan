b2e6e4c2fd7dd188bbcdfee3568b2745be1c60b6
Bartłomiej Gordon <bartekk.gordon@gmail.com>
2023-01-24 21:56:30 +0100

    Add unit tests to check dimensions on nifit file

--- BrainScanTests/test.cpp
+++ BrainScanTests/test.cpp
@@ -2,11 +2,34 @@
 #include "scan/Scan.hpp"
 #include "Core.hpp"
 
-TEST(TestScanDimensions, ScanMockDataTests)
+inline int GenerateDimensionCount(View& view)
+{
+	return (view.GetDepth() * view.GetWidth() * view.GetHeight());
+}
+
+TEST(ScanDimensions, TestScanDimensionsAxial)
 {
     Scan scan;
     scan.CreateMockData();
-    int atLeastThreeDims = scan.GetAxial()->GetDepth() * scan.GetAxial()->GetWidth() * scan.GetAxial()->GetHeight();
+	int atLeastThreeDims = GenerateDimensionCount(*scan.GetAxial());
 
     EXPECT_GT(atLeastThreeDims, 0);
+}
+
+TEST(ScanDimensions, TestScanDimensionsSagittal)
+{
+	Scan scan;
+	scan.CreateMockData();
+	int atLeastThreeDims = GenerateDimensionCount(*scan.GetSagittal());
+
+	EXPECT_GT(atLeastThreeDims, 0);
+}
+
+TEST(ScanDimensions, TestScanDimensionsCoronal)
+{
+	Scan scan;
+	scan.CreateMockData();
+	int atLeastThreeDims = GenerateDimensionCount(*scan.GetCoronal());
+
+	EXPECT_GT(atLeastThreeDims, 0);
 }
