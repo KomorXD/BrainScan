11a6492ddc795f688c243758d6fa3d786ef8dc36
Bartłomiej Gordon <bartekk.gordon@gmail.com>
2023-01-24 22:17:15 +0100

    Add unit tests to check if all values are normalized

--- BrainScanTests/test.cpp
+++ BrainScanTests/test.cpp
@@ -7,7 +7,7 @@ inline int GenerateDimensionCount(View& view)
 	return (view.GetDepth() * view.GetWidth() * view.GetHeight());
 }
 
-TEST(ScanDimensions, TestScanDimensionsAxial)
+TEST(ScanDimensionsTest, TestScanDimensionsAxial)
 {
     Scan scan;
     scan.CreateMockData();
@@ -16,7 +16,7 @@ TEST(ScanDimensions, TestScanDimensionsAxial)
     EXPECT_GT(atLeastThreeDims, 0);
 }
 
-TEST(ScanDimensions, TestScanDimensionsSagittal)
+TEST(ScanDimensionsTest, TestScanDimensionsSagittal)
 {
 	Scan scan;
 	scan.CreateMockData();
@@ -25,11 +25,52 @@ TEST(ScanDimensions, TestScanDimensionsSagittal)
 	EXPECT_GT(atLeastThreeDims, 0);
 }
 
-TEST(ScanDimensions, TestScanDimensionsCoronal)
+TEST(ScanDimensionsTest, TestScanDimensionsCoronal)
 {
 	Scan scan;
 	scan.CreateMockData();
 	int atLeastThreeDims = GenerateDimensionCount(*scan.GetCoronal());
 
 	EXPECT_GT(atLeastThreeDims, 0);
+}
+
+::testing::AssertionResult AreAllValuesBetweenInclusive(View* view, int from, int to)
+{
+	std::vector<float*> data = view->GetData();
+	for (int i = 0; i < data.size(); i++)
+	{
+		for (size_t j = 0; j < view->GetDepth(); j++)
+		{
+			if(!(data[j][i] >= from) && (data[j][i] <= to))
+				return ::testing::AssertionFailure() << data[j][i] << " is outside the range " << from << " to " << to << " (Layer: "<<j<<" Buffer index: "<<i;
+		}
+	}
+	return ::testing::AssertionSuccess();
+}
+
+TEST(PixelValueTest, CheckPixelValuesRangeFrom0To1Axial)
+{
+	Scan scan;
+	scan.CreateMockData();
+	View* axial = scan.GetAxial();		
+
+	EXPECT_TRUE(AreAllValuesBetweenInclusive(axial, 0, 1));
+}
+
+TEST(PixelValueTest, CheckPixelValuesRangeFrom0To1Sagittal)
+{
+	Scan scan;
+	scan.CreateMockData();
+	View* axial = scan.GetSagittal();
+
+	EXPECT_TRUE(AreAllValuesBetweenInclusive(axial, 0, 1));
+}
+
+TEST(PixelValueTest, CheckPixelValuesRangeFrom0To1Coronal)
+{
+	Scan scan;
+	scan.CreateMockData();
+	View* axial = scan.GetCoronal();
+
+	EXPECT_TRUE(AreAllValuesBetweenInclusive(axial, 0, 1));
 }
