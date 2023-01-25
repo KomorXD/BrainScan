61cfaebdc7be403c6353e30e4190359ca38299e7
KomorXD <pawel.kupczak.sc@gmail.com>
2023-01-25 15:06:46 +0100

    Add new pure virtual method

--- BrainScan/src/scenes/IScene.hpp
+++ BrainScan/src/scenes/IScene.hpp
@@ -1,9 +1,13 @@
 #pragma once
 
+#include "../ui/UIPanels.hpp"
+
 class IScene
 {
 	public:
 		virtual ~IScene() = default;
+
+		virtual PathsPack RequestPathsPack() = 0;
 
 		virtual void Input()   = 0;
 		virtual void Update()  = 0;
