fe9aee978cdaedf4377ce5aa3b578fcfd78ac539
Bartłomiej Gordon <bartekk.gordon@gmail.com>
2023-01-26 00:50:03 +0100

    Disable console in Prod configuration

--- BrainScan/src/Main.cpp
+++ BrainScan/src/Main.cpp
@@ -1,8 +1,13 @@
 #include "App.hpp"
 #include "Core.hpp"
+#include <Windows.h>
 
 int main(int argc, char** argv)
 {
+#ifdef BS_PROD
+	ShowWindow(GetConsoleWindow(), SW_HIDE);
+#endif
+
 	Logger::Init();
 	
 	App::GetInstance().Run();	
