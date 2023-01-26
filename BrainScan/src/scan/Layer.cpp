76d8532446653190dd2f47b75372198518e9a2fd
KomorXD <pawel.kupczak.sc@gmail.com>
2023-01-26 17:54:03 +0100

    Adjust to new rendering strat

--- BrainScan/src/scan/Layer.cpp
+++ BrainScan/src/scan/Layer.cpp
@@ -2,10 +2,10 @@
 
 std::unique_ptr<VertexArray>  Path::s_PathVAO;
 std::unique_ptr<VertexBuffer> Path::s_PathVBO;
-std::unique_ptr<IndexBuffer>  Path::s_PathIBO;
 std::unique_ptr<Shader>		  Path::s_PathsShader;
 
 float Path::s_Color[3]{1.0f, 0.0f, 0.0f};
+float Path::s_Radius = 1.0f;
 
 void Path::InitializeBuffers()
 {
@@ -13,15 +13,14 @@ void Path::InitializeBuffers()
 
 	s_PathVAO = std::make_unique<VertexArray>();
 	s_PathVBO = std::make_unique<VertexBuffer>(nullptr, 0);
-	s_PathIBO = std::make_unique<IndexBuffer>(nullptr, 0);
 
 	VertexBufferLayout layout;
 
 	layout.Push<float>(2);
 	layout.Push<float>(3);
+	layout.Push<float>(1);
 
 	s_PathVAO->AddBuffer(*Path::s_PathVBO, layout);
 	s_PathVAO->Unbind();
 	s_PathVBO->Unbind();
-	s_PathIBO->Unbind();
 }
